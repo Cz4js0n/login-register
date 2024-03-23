59c32c873597486d4d8eed26857643a37280ccd2
Czajson <119066425+Cz4js0n@users.noreply.github.com>
2024-03-24 00:24:04 +0100

    Delete stworzenie_bazy.sql

--- stworzenie_bazy.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE users;
-USE users;
-
-CREATE TABLE user_data (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL
-);
