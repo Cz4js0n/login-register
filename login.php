0146699f44da56efdd2ee0d498229144088b8e9f
Czajson <119066425+Cz4js0n@users.noreply.github.com>
2024-03-24 00:20:51 +0100

    Update login.php

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
 
     function poprawnyEmail($email) 
     {
-        return preg_match('/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$//', $email);
+        return preg_match('/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/', $email);
     }
 
     function poprawneHaslo($haslo) 
